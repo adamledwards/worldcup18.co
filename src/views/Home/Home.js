f61bee1fbb5ddd08389988626be43177932021a8
Adam Edwards <adamedwards88@gmail.com>
2019-03-15 15:06:15 +0000

    remove todays fixtures

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -12,7 +12,6 @@ class Home extends Component {
     return (
       <Fragment>
         <Header />
-        <TodaysFixtures />
         <FixturesResult />
         <Groups />
         <TopScorers />
